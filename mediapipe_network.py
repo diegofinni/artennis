aebbc24346f0e6b04c4d2c42f84e751325e85ed3
diegofinni <diegofinni@gmail.com>
2022-05-04 21:18:27 -0400

    Final commit MP

--- mediapipe_network.py
+++ mediapipe_network.py
@@ -19,6 +19,8 @@ def sendRoutine(piClient: PiClient):
         time.sleep(1)
     while piClient.running:
         mediapipe_pose_est.mediapipe_pose_est(piClient)
+        piClient.running = False
+        return
 
 def recvRoutine(piClient: PiClient):
     global first_packet
@@ -34,20 +36,32 @@ def recvRoutine(piClient: PiClient):
                 # print(curr_time - packet_second*1000000 - packet_microsecond)
                 # time.sleep(1000)
             else:
-                print("RECEIVED: ", packet)
-                mediapipe_pose_est.ballRecieved(packet)
+                if (packet.ballX != 10000):
+                    print("RECEIVED: ", packet)
+                if (mediapipe_pose_est.ballRecieved(packet)):
+                    return
         except Exception as e:
             print(e)
     
 def main():
+    myAddr = ""
+    myPort = 0
+    otherAddr = ""
+    otherPort = 0
     if len(sys.argv) != 5:
-        print("Usage: myAddr, myPort, otherAddr, otherPort")
-        exit()
-    
-    myAddr = sys.argv[1]
-    myPort = int(sys.argv[2])
-    otherAddr = sys.argv[3]
-    otherPort = int(sys.argv[4])
+        print("Default networking")
+        myAddr = "172.26.165.132"
+        myPort = 6666
+        otherAddr = "172.26.165.64"
+        otherPort = 5555
+        # print("Usage: myAddr, myPort, otherAddr, otherPort")
+        # exit()
+    else: 
+        print("Using argv for networking")
+        myAddr = sys.argv[1]
+        myPort = int(sys.argv[2])
+        otherAddr = sys.argv[3]
+        otherPort = int(sys.argv[4])
 
     pi = PiClient(myAddr, myPort, otherAddr, otherPort)
     pi.setSendRoutine(sendRoutine)
--- mediapipe_pose_est.py
+++ mediapipe_pose_est.py
@@ -10,9 +10,70 @@ from typing import Optional, Tuple, NamedTuple
 from piclient import PiClient
 from gamepacket import GamePacket
 
+
+class ANamedTuple(NamedTuple):
+    x: int
+    y: int
+
+class Ball:
+    ball = cv2.imread(r'ball.png', cv2.IMREAD_UNCHANGED)
+    ballImage = 0
+    ballAlpha = 0
+    ballDim = 0
+    # parameterized constructor
+    def __init__(self, scale_percent):
+        self.ballDim = ((int(self.ball.shape[1] * scale_percent / 100)), int(self.ball.shape[0] * scale_percent / 100))
+        self.ballImage = cv2.resize(self.ball, self.ballDim, interpolation = cv2.INTER_AREA)
+        self.ballAlpha = self.ballImage[..., 3] / 255.0
+        self.ballAlpha = np.repeat(self.ballAlpha[..., np.newaxis], 3, axis=2)
+        self.ballImage = self.ballImage[..., :3]
+
+
+
+win = False
+scores = [0, 0]
+advantages = [False, False]
+ballx = .1
+bally = .1
+# opponentImage = np.zeros((720, 1280))
+# opponentImage = cv2.cvtColor(opponentImage, cv2.COLOR_GRAY2BGR)
+opponentImage = np.zeros((720, 1280, 3), np.uint8)
+counting_down = False
+counting_start = 0
+record_pose = False
+ball_received = False
+displayBall = True
+
+def incrament_score(i):
+  global scores, advantages, win
+
+  if (scores[i] == 0):
+    scores[i] = 15
+  elif (scores[i] == 15):
+    scores[i] = 30
+  elif (scores[i] == 30):
+    scores[i] = 40
+  elif (scores[i] == 40):
+    if (scores[0] == scores[1]):
+      if (advantages[i] == True):
+        win = True
+        return True
+      elif (advantages[(i+1)%2] == True):
+        advantages[(i+1)%2] = False
+      else:
+        advantages[i] = True
+    else:
+      win = True
+      return True
+  return False
+
+
 def physicsCalc(init_pose, final_pose, deltaT):
+  global ballx, bally
+  print(ballx, bally, init_pose.x, init_pose.y, init_pose.x*1280, init_pose.y*720)
+  if ((init_pose.x*1280 + 250 < 1280*ballx or init_pose.x*1280 - 250 > 1280*ballx) or (init_pose.y*720 + 250 < 720*bally or init_pose.y*720 - 250 > 720*bally)):
+    return True, -1, -1
   # physics
-  print()
 
   # Window frame dimensions (adjust for 2m by 2m)
   width = 2 # m scale to human position (window width is 1920 pixels)
@@ -55,64 +116,58 @@ def physicsCalc(init_pose, final_pose, deltaT):
   finalX = init_pose.x + deltaX/width
   finalY = init_pose.y - deltaY/height
 
-  return finalX, finalY
+  return False, finalX, finalY
 
-class ANamedTuple(NamedTuple):
-    x: int
-    y: int
-
-class Ball:
-    ball = cv2.imread(r'ball.png', cv2.IMREAD_UNCHANGED)
-    ballImage = 0
-    ballAlpha = 0
-    ballDim = 0
-    # parameterized constructor
-    def __init__(self, scale_percent):
-        self.ballDim = ((int(self.ball.shape[1] * scale_percent / 100)), int(self.ball.shape[0] * scale_percent / 100))
-        self.ballImage = cv2.resize(self.ball, self.ballDim, interpolation = cv2.INTER_AREA)
-        self.ballAlpha = self.ballImage[..., 3] / 255.0
-        self.ballAlpha = np.repeat(self.ballAlpha[..., np.newaxis], 3, axis=2)
-        self.ballImage = self.ballImage[..., :3]
-
-ballx = 0
-bally = 0
-# opponentImage = np.zeros((720, 1280))
-# opponentImage = cv2.cvtColor(opponentImage, cv2.COLOR_GRAY2BGR)
-opponentImage = np.zeros((720, 1280, 3), np.uint8)
-counting_down = False
-counting_start = 0
-record_pose = False
-ball_received = False
-displayBall = True
+def reset():
+  global ballx, bally, opponentImage, counting_down, counting_start, record_pose, ball_received, displayBall
+  ballx = .1
+  bally = .1
+  opponentImage = np.zeros((720, 1280, 3), np.uint8)
+  counting_down = False
+  counting_start = 0
+  record_pose = False
+  ball_received = False
+  displayBall = True
 
 def ballRecieved(packet: GamePacket):
-  global ballx, bally, opponentImage, counting_down, counting_start, record_pose, ball_received
+  global ballx, bally, opponentImage, counting_down, counting_start, record_pose, ball_received, win
+  if (win):
+      return True
   if (packet.ballX == 10000 and not counting_down and not record_pose and ball_received): # done receiving
     counting_down = True
     counting_start = time.time()
+  elif packet.ballX == 20000:
+    end = incrament_score(0)
+    if end:
+      win = True
+    reset()
   elif packet.ballX != 10000:
-    displayBall = False
-    ball_received = True
-    ballx = packet.ballX/opponentImage.shape[1]
-    bally = packet.ballY/opponentImage.shape[0]
+    if packet.ballX != 30000:
+      displayBall = False
+      ball_received = True
+      ballx = packet.ballX/opponentImage.shape[1]
+      bally = packet.ballY/opponentImage.shape[0]
     xmin = packet.minX
     xmax = packet.maxX
     ymin = packet.minY
     ymax = packet.maxY
-    hscale = 1
-    vscale = 1
-    opponentImage = np.zeros((720, 1280, 3), np.uint8)
-    box_start = (int(xmin * hscale), int(ymin *vscale))
-    box_end = (int(xmax *hscale), int(ymax*vscale))
-    txt_start = (int(xmin *hscale), int(ymin *vscale)-5)
-    color = (255, 255, 255)
-    opponentImage = cv2.rectangle(opponentImage, box_start, box_end, color, 5)
+    hscale = 1280/416
+    vscale = 720/416
+    if (xmin != 0 and ymin != 0):
+      opponentImage = np.zeros((720, 1280, 3), np.uint8)
+      box_start = (int(xmin * hscale), int(ymin *vscale))
+      box_end = (int(xmax *hscale), int(ymax*vscale))
+      # txt_start = (int(xmin *hscale), int(ymin *vscale)-5)
+      color = (255, 255, 255)
+      opponentImage = cv2.rectangle(opponentImage, box_start, box_end, color, 5)
+      # opponentImage = cv2.flip(opponentImage, 1)
+  return False
 
 
 
 def mediapipe_pose_est(piClient: PiClient):
   # Initialize function global variables
-  global ballx, bally, opponentImage, counting_down, counting_start, record_pose, ball_received, displayBall
+  global ballx, bally, opponentImage, counting_down, counting_start, record_pose, ball_received, displayBall, win
   val = 0
   # Init Pose Bool
   init_pose_bool = False    
@@ -129,20 +184,27 @@ def mediapipe_pose_est(piClient: PiClient):
   cam.set(3, 1280)
   cam.set(4, 720)
 
-  # cv2.namedWindow("Window", cv2.WND_PROP_FULLSCREEN)
-  # cv2.setWindowProperty("Window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+  cv2.namedWindow("Frame", cv2.WND_PROP_FULLSCREEN)
+  cv2.setWindowProperty("Frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
+      
+# Main Loop
+  frame_start_time = time.time()
+  record_start = 0
   with mp_pose.Pose(
       min_detection_confidence=0.5,
       min_tracking_confidence=0.5,
       model_complexity=0,
       smooth_landmarks=True) as pose:
-    
-    # Main Loop
-    frame_start_time = time.time()
     while cam.isOpened():
       success, image = cam.read() #Read Camera Frame
-      FPS = round(1/(time.time() - frame_start_time))
+      # FPS
+      FPS = round(2/(time.time() - frame_start_time))
+      # Latency
+      if win:
+        cv2.destroyAllWindows()
+        return
+      mediapipe_total_latency = -1
       frame_start_time = time.time()
       ball = ball2_5.ballImage
       ball_alpha = ball2_5.ballAlpha
@@ -157,40 +219,50 @@ def mediapipe_pose_est(piClient: PiClient):
 
       # SpaceBar is hit let's annotate pose on the image.
       if val == 32 and not record_pose:
-          counting_down = True # if sendOrRecieve is Recieve
-          counting_start = time.time()
+        counting_down = True # if sendOrRecieve is Recieve
+        counting_start = time.time()
       image.flags.writeable = True 
       if counting_down:
-          displayBall = True
-          time_diff = round(time.time() - counting_start)
-          if time_diff == 0:
-              ball = ball10.ballImage
-              ball_alpha = ball10.ballAlpha
-              dim = ball10.ballDim
-          elif time_diff == 1:
-              ball = ball5.ballImage
-              ball_alpha = ball5.ballAlpha
-              dim = ball5.ballDim
+        displayBall = True
+        time_diff = round(time.time() - counting_start)
+        if time_diff == 0:
+          ball = ball10.ballImage
+          ball_alpha = ball10.ballAlpha
+          dim = ball10.ballDim
+        elif time_diff == 1:
+          ball = ball5.ballImage
+          ball_alpha = ball5.ballAlpha
+          dim = ball5.ballDim
 
-          if time_diff >= 3:
-              record_pose = True
-              record_start = time.time()
-              counting_down = False
+        if time_diff >= 3:
+          record_pose = True
+          counting_down = False
 
       # We want to record the pose data
       if record_pose:
+        #Latency
+        FPS //= 2
+        mediapipe_latency_start = time.time()
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
         results = pose.process(image)
         mp_drawing.draw_landmarks(
             image,
             results.pose_landmarks,
             mp_pose.POSE_CONNECTIONS,
             landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+
+        if (results == None or results.pose_landmarks == None):
+          continue
+        
+        #Latency
+        mediapipe_total_latency = time.time()-mediapipe_latency_start
         if not init_pose_bool:
+          record_start = time.time()
           init_pose = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST]
           current_pose = init_pose
-          init_pose_bool = True 
+          init_pose_bool = True
         # Flip the image horizontally for a selfie-view display.
         deltaT = time.time() - record_start
         current_pose = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST]
@@ -203,19 +275,33 @@ def mediapipe_pose_est(piClient: PiClient):
         packet.maxX *= image.shape[1]
         packet.maxY *= image.shape[0]
 
-        packet.ballX = round(packet.ballX)
-        packet.ballY = round(packet.ballY)
-        packet.minX = round(max(0, packet.minX-50)) 
-        packet.minY = round(max(0, packet.minY-50))
-        packet.maxX = round(min(image.shape[1], packet.maxX+50))
-        packet.maxY = round(min(packet.maxY+50, image.shape[0]))
+        packet.ballX = 30000
+        packet.ballY = 30000
+        packet.minX = round(max(0, packet.minX-125)) 
+        packet.minY = round(max(0, packet.minY-125))
+        packet.maxX = round(min(image.shape[1], packet.maxX+125))
+        packet.maxY = round(min(packet.maxY+125, image.shape[0]))
         piClient.sendPacket(packet)
-        print("SENDING: ", packet)
+        if (packet.ballX != 10000):
+            print("SENDING: ", packet)
         if deltaT > 2:
           final_pose = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST]
           record_pose = False
           init_pose_bool = False
-          ballx, bally = physicsCalc(init_pose,final_pose, deltaT)
+          miss, tmp_ballx, tmp_bally = physicsCalc(init_pose,final_pose, deltaT)
+          if miss:
+            end = incrament_score(1)
+            packet = GamePacket(20000, 20000, 20000, 20000, 20000, 20000)
+            piClient.sendPacket(packet)
+            if (end == True):
+              win = True
+              cv2.destroyAllWindows()
+              return
+            reset()
+            continue
+          else:
+            ballx = tmp_ballx
+            bally = tmp_bally
           packet = GamePacket(ballx, bally, minX=current_pose.x, maxX=current_pose.x,
                               minY=current_pose.y, maxY=current_pose.y)
           packet.ballX *= image.shape[1]
@@ -227,13 +313,14 @@ def mediapipe_pose_est(piClient: PiClient):
 
           packet.ballX = round(packet.ballX)
           packet.ballY = round(packet.ballY)
-          packet.minX = round(max(0, packet.minX-50)) 
-          packet.minY = round(max(0, packet.minY-50))
-          packet.maxX = round(min(image.shape[1], packet.maxX+50))
-          packet.maxY = round(min(packet.maxY+50, image.shape[0]))
+          packet.minX = round(max(0, packet.minX-125)) 
+          packet.minY = round(max(0, packet.minY-125))
+          packet.maxX = round(min(image.shape[1], packet.maxX+125))
+          packet.maxY = round(min(packet.maxY+125, image.shape[0]))
           piClient.sendPacket(packet)
           piClient.sendPacket(packet)
-          print("SENDING: ", packet)
+          if (packet.ballX != 10000):
+            print("SENDING: ", packet)
           ball_received = False
           displayBall = False
       else:
@@ -253,17 +340,23 @@ def mediapipe_pose_est(piClient: PiClient):
         opponent_image[ey:ey+dim[1], ex:ex+dim[0], :] = opponent_image[ey:ey+dim[1], ex:ex+dim[0], :] * (1 - ball_alpha) + ball * ball_alpha
       image = cv2.flip(image,1)
       opponent_image = cv2.flip(opponent_image, 1)
+      image = cv2.putText(image, "P1: {} Adv: {}".format(scores[0], advantages[0]), (15, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 6, cv2.LINE_AA)
+      opponent_image = cv2.putText(opponent_image, "P2: {} Adv: {}".format(scores[1], advantages[1]), (15, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 6, cv2.LINE_AA)
+      image = cv2.putText(image, str(FPS), (15, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 6, cv2.LINE_AA)
+      image = cv2.putText(image, str(mediapipe_total_latency), (15, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 6, cv2.LINE_AA)
       if counting_down:
         image = cv2.putText(image,str(3 - round(time.time() - counting_start)), (image.shape[1]//2-200, image.shape[0]//2+100), cv2.FONT_HERSHEY_SIMPLEX, 16, (255, 0, 0), 70, cv2.LINE_AA)
       # Display annotated image
-      # image = cv2.putText(image, str(FPS), (10, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 6, cv2.LINE_AA)
       image = cv2.resize(image, (image.shape[1]//2, image.shape[0]//2), interpolation = cv2.INTER_AREA)
       opponent_image = cv2.resize(opponent_image, (opponent_image.shape[1]//2, 
                                                    opponent_image.shape[0]//2), 
                                   interpolation = cv2.INTER_AREA)
       image = cv2.hconcat([image, opponent_image])
-      cv2.imshow('MediaPipe Pose', image)
+      image = cv2.resize(image,(2560, 720), interpolation=cv2.INTER_AREA)
+      cv2.imshow('Frame', image)
       val = cv2.waitKey(1)
+      if win:
+          cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     mediapipe_pose_est()
